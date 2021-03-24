0c27a3f2929615ab2b5b9fd4297027a1927e87e3
Akihiro Tanaka <akihiro.fin@icloud.com>
2021-03-24 18:40:25 +0200

    Viikko 2 laskarit t.2

--- laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
+++ laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
@@ -7,17 +7,3 @@ class TestMaksukortti(unittest.TestCase):
 
     def test_luotu_kortti_on_olemassa(self):
         self.assertNotEqual(self.maksukortti, None)
-
-    def test_syo_maukkaasti_ei_vie_saldoa_negatiiviseksi(self):
-        self.maksukortti.syo.maukkaasti()
-        self.maksukortti.syo.maukkaasti()
-        self.maksukortti.syo.maukkaasti()
-        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 2 euroa")
-    
-    def test_ei_lataa_negatiivista_rahamaaraa(self):
-        self.maksukortti.lataa_rahaa(-10)
-        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 10 euroa")
-    
-    def test_syo_edullisesti_vain_jos_rahaa_riittavasti(self):
-        self.maksukortti.syo.edullisesti()
-        self.maksukortti.syo.edullisesti()
